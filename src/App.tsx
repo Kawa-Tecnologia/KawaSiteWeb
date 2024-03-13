8e4e0626a7a3378eedc2184a38ace38eee186709
rohidekiever <rodrigo.hideki@evermart.com.br>
2024-03-13 11:08:55 -0300

    adjust variable

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ const App: React.FC = () => {
     const fetchDevelopers = async () => {
       try {
         const { data } = await axios.get(
-          `https://kawa-site-web-ten.vercel.app/api/user?type=platform`,
+          `https://kawa-site-web-back.vercel.app/api/user?type=platform`,
           {
             headers: {
               Authorization: `Bearer ${process.env.REACT_APP_TOKEN_DEV}`
