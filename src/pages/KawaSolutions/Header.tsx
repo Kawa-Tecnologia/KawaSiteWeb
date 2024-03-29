2ee8e2dc45a4e868e04c6de455f642ac0357cd46
rohidekiever <rodrigo.hideki@evermart.com.br>
2024-03-29 13:57:17 -0300

    adjust solutions

--- src/pages/KawaSolutions/Header.tsx
+++ src/pages/KawaSolutions/Header.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
     <header style={styles.header}>
       <div style={styles.content}>
-        <h1>Kawa Solutions</h1>
-        <p>
+        <h1 style={styles.heading}>Kawa Solutions</h1>
+        <p style={styles.paragraph}>
           Bem-vindo ao nosso site! Oferecemos uma variedade de serviços de
           desenvolvimento para atender às suas necessidades.
         </p>
       </div>
     </header>
-  )
-}
+  );
+};
 
 const styles = {
   header: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    background: '#f0f0f0'
+    background: '#333', 
+    color: '#fff', 
   },
   content: {
-    textAlign: 'center'
-  } as React.CSSProperties
-}
+    textAlign: 'center',
+  }as React.CSSProperties,
+  heading: {
+    color: '#0066CC',
+    fontSize: '2rem', 
+    marginBottom: '0.5rem', 
+  },
+  paragraph: {
+    fontSize: '1rem',
+    margin: '0', 
+  },
+};
 
-export default Header
+export default Header;
