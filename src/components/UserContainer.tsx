f520e7e0255b49819b5aec79cfad5055473a686e
rohidekiever <rodrigo.hideki@evermart.com.br>
2024-04-03 19:22:26 -0300

    add socket for points updated

--- src/components/UserContainer.tsx
+++ src/components/UserContainer.tsx
@@ -5,7 +5,7 @@ import '../assets/styles/PointsModal.css'
 import '../assets/styles/UserContainer.css'
 import FuturisticModal from './FuturistModal'
 import { Star } from '@material-ui/icons'
-//import { io } from 'socket.io-client'
+import { io } from 'socket.io-client'
 interface UserData {
   id: number
   name: string
@@ -30,12 +30,9 @@ const UserContainer: React.FC<UserContainerProps> = ({ handleLogout }) => {
   const navigate = useNavigate()
   const userName = localStorage.getItem('userName') || ''
   const devTag = localStorage.getItem('tagName') || ''
-  const points
-      //setPoints]
-     = 
-     //useState<string>(
+  const [points, setPoints] = useState<string>(
     localStorage.getItem('userPoints') || '0'
-  //)
+  )
   useEffect(() => {
     const pointsElement = document.getElementById('user-points')
     if (pointsElement) {
@@ -56,14 +53,15 @@ const UserContainer: React.FC<UserContainerProps> = ({ handleLogout }) => {
     }
   }, [])
 
-  // useEffect(() => {
-  //   const socket = io(`${process.env.REACT_APP_API_URL}`)
-  //   socket.on('points', (user_id, points_acquired) => {
-  //     if(user_id === user?.id){
-  //     setPoints(points + points_acquired)
-  //     }
-  //   })
-  // }, [])
+  useEffect(() => {
+    const socket = io(`${process.env.REACT_APP_API_URL}`)
+    socket.on('pointsUpdated', ({userId, pointsTotal}) => {
+      if (userId === user?.id) {
+        setPoints(pointsTotal.toString())
+        console.log('Pontos Atualizados')
+      }
+    })
+  }, [])
   const handleAcquirePoints = () => {
     setModalIsOpen(true)
   }
